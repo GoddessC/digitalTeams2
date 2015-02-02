21108ae621116e0b9892e58744d6d7b962d662d5
GoddessC <goddesselixir@gmail.com>
2015-02-01 23:48:04 -0500

    Fixes

---
+++ hookers/assets/js/data.js
@@ -0,0 +1,25 @@
+// DATA
+
+var sites = [
+	// GENERAL HOOKER
+	{
+		// above the fold
+		loadHeader: "ROUGH AND REFINED",
+		loadImage: "assets/img/hero-general.png",
+		loadTitle: "Good Day",
+		loadBackground: "url('assets/img/wood-bg.jpg')",
+		loadCopy: "Good day fine ladies and esteemed gentlemen.  I am Major General Joseph Hooker and it is my great pleasure to introduce Hooker’s House Bourbon: battle-tested bourbon with the smooth sophistication of wine country.  But do not underestimate us!  My bourbon and I are here to disrupt and awaken your dusty wine traditions. ",
+		loadCopy2: "Once you’ve sipped upon my wine barrel-aged Kentucky bourbon- with its subtle hint of pinot noir, drinking wine will never feel the same.  Every sip of Hookers House Bourbon should be taken in like an orchestral overture.  Let the warmth and smooth flavor wash over your palette dear drinker, and bask in the glow of the love child of Kentucky and California."
+	},
+
+	// FIGHTING JOE
+	{
+		loadHeader: "FIGHTING JOE",
+		loadImage: "assets/img/fighting-joe-x.png",
+		loadTitle: "This Is Different",
+		loadBackground: "url('assets/img/barrel-bg.jpg')",
+		loadCopy: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga consectetur corporis sed consequatur magni itaque non pariatur reprehenderit dolores. Minima illum non, dolore omnis iure dolorem quam odio ullam quis?",
+		loadCopy2: "My bourbon and I are here to disrupt and awaken your dusty wine traditions, my esteemed colleagues. Trust in me: once you’ve sipped upon my wine barrel - aged Kentucky bourbon - with its subtle hint of pinot noir, drinking wine will never feel the same.  Every sip of Hookers House Bourbon should be taken in like an orchestral overture.  Let the warmth and smooth flavor wash over your palette dear drinker, and bask in the glow of the love child of Kentucky and California.."
+	}
+];
+
